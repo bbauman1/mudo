8b929cfa49f0b75e681f1f2660203bde481133c3
Brett Bauman <bauman.brett3@gmail.com>
2021-12-22 12:59:38 -0800

    add temp splash screen

--- mudo/ContentView.swift
+++ mudo/ContentView.swift
@@ -10,10 +10,19 @@ import SwiftUI
 struct ContentView: View {
     
     @AppStorage("appColor") var appColor: AppColor = .default
+    @State var isSplashScreenFinished: Bool = false
     
     let moodStore = MoodStore()
     
     var body: some View {
+        if isSplashScreenFinished {
+            tabBarView
+        } else {
+            splashScreen
+        }
+    }
+    
+    var tabBarView: some View {
         TabView {
             TodayView(viewModel: TodayViewModel(moodStore: moodStore))
                 .tabItem {
@@ -27,6 +36,18 @@ struct ContentView: View {
         }
         .accentColor(appColor.color)
         .tint(appColor.color)
+    }
+    
+    var splashScreen: some View {
+        Text("mudo (wip)")
+            .font(.system(size: 36, weight: .bold, design: .rounded))
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                    withAnimation {
+                        isSplashScreenFinished = true
+                    }
+                }
+            }
     }
 }
 
