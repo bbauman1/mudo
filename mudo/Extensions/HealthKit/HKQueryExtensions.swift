5e16396a55a15e0f5b707bf5e56fb73a6bca6eae
Brett Bauman <bauman.brett3@gmail.com>
2022-03-24 10:34:16 -0700

    fix bug with pulling health data for previous days

--- mudo/Extensions/HealthKit/HKQueryExtensions.swift
+++ mudo/Extensions/HealthKit/HKQueryExtensions.swift
@@ -12,6 +12,11 @@ extension HKQuery {
     
     static func predicateForSamples(within date: Date) -> NSPredicate {
         let startOfDay = Calendar.current.startOfDay(for: date)
-        return predicateForSamples(withStart: startOfDay, end: date, options: .strictStartDate)
+        if let nextDay = Calendar.current.date(byAdding: .day, value: 1, to: date) {
+            let endOfDay = Calendar.current.startOfDay(for: nextDay)
+            return predicateForSamples(withStart: startOfDay, end: endOfDay, options: .strictStartDate)
+        } else {
+            return predicateForSamples(withStart: startOfDay, end: date, options: .strictStartDate)
+        }
     }
 }
